ad185b0e988de5487fd81ea891f73c2e40550b27
danielyou0230 <danielyou.ee03@g2.nctu.edu.tw>
2018-02-20 21:52:23 -0600

    add multithreading

--- src/recognize_sentences.py
+++ src/recognize_sentences.py
@@ -4,6 +4,26 @@ import json
 from pprint import pprint
 from mmap import mmap
 from tqdm import tqdm
+import multiprocessing
+from multiprocessing import Pool
+
+
+keywords = None
+
+def threading_search(thread_idx, data):
+    global keywords
+    result = list()
+    #
+    for line in tqdm(data, position=thread_idx):
+        # search for keywords
+        for itr in keywords:
+            if itr in line:
+                # print(" - Found sentence with keyword: {0}".format(itr))
+                result.append(line)
+                break
+            else:
+                pass
+    return result
 
 def get_num_lines(file_path):
     lines = 0
@@ -14,7 +34,6 @@ def get_num_lines(file_path):
     return lines
 
 def recognize_sentences(args):
-    total_lines = get_num_lines(args.sentences)
     with open(args.sentences, "r") as f_in, open(args.found, "w") as f_out:
         # load dictionarys
         files = [itr for itr in os.listdir(args.dict_path) 
@@ -24,8 +43,33 @@ def recognize_sentences(args):
         for itr in files:
             entity.update(json.load(open(args.dict_path + '/' + itr, "r")))
         #
+        global keywords
         keywords = entity.keys()
         count = 0
+        #
+        raw_data = f_in.read().splitlines()
+        n_cores = multiprocessing.cpu_count()
+        n_threads = n_cores * 2 if args.thread == None else args.thread
+        print("Number of cores available: {:d}".format(n_cores))
+        print("Number of threads opended: {:d}".format(n_threads))
+        # Slice data
+        thread_data = list()
+        per_slice = int(len(raw_data) / n_threads)
+        for itr in range(n_threads):
+            idx_begin = itr * per_slice
+            idx_end = (itr + 1) * per_slice if itr != n_threads - 1 else None
+            #
+            thread_data.append((itr, raw_data[idx_begin:idx_end]))
+        # Threading
+        # pool = ThreadPool(n_threads)
+        with Pool(processes=n_threads) as p:
+            result = p.starmap(threading_search, thread_data)
+
+        # write to file
+        for per_thread in result:
+            for line in per_thread:
+                f_out.write(line + "\n")
+        """
         for line in tqdm(f_in, total=total_lines):
             # search for keywords
             for itr in keywords:
@@ -35,6 +79,7 @@ def recognize_sentences(args):
                     break
                 else:
                     pass
+        """
 
 
 if __name__ == '__main__':
@@ -43,6 +88,8 @@ if __name__ == '__main__':
     parser.add_argument("found", help="Sentences with key words")
     parser.add_argument("dict_path", help="Put all dictionaries \
                          with extension \".json\".")
+    parser.add_argument("-t", "--thread", type=int, help="Number of threads \
+                        to run, default: 2 * number_of_cores") 
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="Verbose output")
 
