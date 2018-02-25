e2ef3aa19d44222adc1f3ca5b7405a04e5c968b9
danielyou0230 <danielyou.ee03@g2.nctu.edu.tw>
2018-02-25 13:13:39 -0600

    modulize threading code

---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import multiprocessing
+from multiprocessing import Pool, cpu_count
+
+def split_data(data, n_slice):
+    """
+    """
+    # Slice data for each thread
+    print(" - Slicing data for threading...")
+    per_slice = int(len(data) / n_slice)
+    partitioned_data = list()
+    for itr in range(n_slice):
+        # Generate indices for each slice
+        idx_begin = itr * per_slice
+        # last slice may be larger or smaller
+        idx_end = (itr + 1) * per_slice if itr != n_slice - 1 else None
+        #
+        partitioned_data.append((itr, data[idx_begin:idx_end]))
+    #
+    return partitioned_data
+
+def generic_threading(n_jobs, data, threading_method):
+    """
+    """
+    # Threading settings
+    n_cores = cpu_count()
+    n_threads = n_cores * 2 if n_jobs == None else n_jobs
+    print("Number of CPU cores: {:d}".format(n_cores))
+    print("Number of Threading: {:d}".format(n_threads))
+    #
+    thread_data = split_data(data, n_threads)
+    #
+    print(" - Begin threading...")
+    # Threading
+    with Pool(processes=n_threads) as p:
+        result = p.starmap(threading_method, thread_data)
+    #
+    print("\n" * n_threads)
+    print("All threads completed.")
+    return result
