083362435445a3ee149f1cfd2c5b058108159579
danielyou0230 <danielyou.ee03@g2.nctu.edu.tw>
2018-02-25 16:52:36 -0600

    added and reorganized refine rules

--- src/refine_corpus.py
+++ src/refine_corpus.py
@@ -1,20 +1,21 @@
 import re
 import argparse
-import json
 from tqdm import tqdm
 from pprint import pprint
 from itertools import chain
+from utils import generic_threading
+
 
 # Define shared objects
 # refine_dict = dict()
-refine_dict = list()
+refine_list = list()
 keys = None
 
 def threading_refine(thread_idx, data):
     """
     """
-    global refine_dict
-    global keys
+    global refine_list
+    # global keys
     #
     result = list()
     for article in tqdm(data, position=thread_idx):
@@ -22,38 +23,31 @@ def threading_refine(thread_idx, data):
         # Find contents within parentheses 
         contents = re.findall(r'\(.*?\)', article)
         for itr_content in contents:
-            for itr_tag in keys:
-                found = re.finall(itr_tag, itr_content)
+            for itr_tag in refine_list:
+                found = re.findall(itr_tag[0], itr_content)
                 if len(found) != 0:
-                    article.replace(itr_content, itr_tag)
+                    article = article.replace(itr_content, itr_tag[1])
                 else:
                     pass
         #
-
         #
         result.append(article)
-
 
     return result
 
 def refine_corpus(args):
     """
     """
-    # load replacement dictionary
-    # refine_dict.update(json.load(open(args.dict_file)))
-    # keys = refine_dict.keys()
+    # load replacement list
+    global refine_list
     with open(args.dict_file) as f_list:
         lines = f_list.read().splitlines()
         for itr in lines:
-            refine_dict.append(itr.split('\t'))
-    print(refine_dict)
+            refine_list.append(itr.split('\t'))
     #
     with open(args.corpus, "r") as f_cor, open(args.output, "w") as f_out:
         # Acquire all sentences
-        raw_data = f_cor.read().splitlines()
-        found = re.findall(refine_dict[-1][0], raw_data[0])
-        print(found)
-        exit()
+        raw_data = f_cor.read().splitlines()[:20]
         # Threading
         result = generic_threading(args.thread, raw_data, threading_refine)
         # write all result to file
@@ -61,7 +55,7 @@ def refine_corpus(args):
         print("Writing result to file...")
         for line in tqdm(list(chain.from_iterable(result))):
             f_out.write(line + "\n")
-        print("File saved in {:s}".format(args.found))
+        print("File saved in {:s}".format(args.output))
 
 
 if __name__ == '__main__':
@@ -78,5 +72,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # check dict file name
-    # assert args.dict_file.endswith(".json")
+    assert args.dict_file.endswith(".tsv")
     refine_corpus(args)
