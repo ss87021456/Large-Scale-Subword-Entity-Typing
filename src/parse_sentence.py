1c163c99f7742d54f0afe5fbb2bb437cf46cf12b
Jack <ss87021456.ee03@g2.nctu.edu.tw>
2018-03-04 23:00:05 -0600

    parallel parse_sentence.py

--- src/parse_sentence.py
+++ src/parse_sentence.py
@@ -1,30 +1,37 @@
 import nltk.data
 import sys, pprint
+from utils import *
 # import numpy as np
 
 filename = sys.argv[1]
+save_name = filename[:-4] + '_sentence.txt'
+thread_num = int(sys.argv[2])
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-with open(filename, 'r') as f:
-	dataset = f.read().splitlines()[1:] # skip first line
-	X = list()
-	count = 0
-	print("start tokenizing...")
-	for data in dataset:
-		if count % 10 == 0:
-			print("step {:3d} / {:3d}".format(count, len(dataset)))
-		result = tokenizer.tokenize(data)
-		tab_index = result[0].find('\t')
-		result[0] = result[0][tab_index+1:] # skip PMID\t
-		#pprint.pprint(result)
-		for element in result:
-			X.append(element)
-		count += 1
 
-	save_name = filename[:-4] + '_sentence.txt'
-	with open(save_name, 'w') as fw:
-		for element in X:
-			fw.write(element+'\n')
 
-	print(len(X))
+def parallel_parse(thread_idx, data):
+    global tokenizer
+    desc = "Thread {:2d}".format(thread_idx + 1)
+    result = list()
+    for article in tqdm(data, position=thread_idx, desc=desc):
+        content = tokenizer.tokenize(article)
+        tab_index = content[0].find('\t')
+        content[0] = content[0][tab_index+1:] # skip PMID\t
+        for element in content:
+            result.append(element)
+
+    return result
+
+
+with open(filename, 'r') as f, open(save_name, "w") as f_out:
+    raw_data = f.read().splitlines()[1:] # skip first line
+    # Threading
+    result = generic_threading(thread_num, raw_data, parallel_parse)
+    save_name = filename[:-4] + '_sentence.txt'
+    # Write all result to file
+    print("Writing result to file...")
+    for line in tqdm(list(chain.from_iterable(result))):
+        f_out.write(line + "\n")
+    print("File saved in {:s}".format(save_name))
 
