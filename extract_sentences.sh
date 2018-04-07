1bac744a8ae289a52dfae21c3bb8c6ad3ee278e8
Jack <ss87021456.ee03@g2.nctu.edu.tw>
2018-04-06 20:16:47 -0500

    remove redundency

--- extract_sentences.sh
+++
@@ -1,2 +0,0 @@
-# python src/recognize_sentences.py data/smaller_sentence.txt data/output.txt data/ --thread=5
-python src/recognize_sentences.py data/smaller_sentence.txt output.txt data/ --thread=5 
