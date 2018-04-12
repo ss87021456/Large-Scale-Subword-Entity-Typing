74178e7426b26c3a33caaec978b332799d9b6289
Jack <ss87021456.ee03@g2.nctu.edu.tw>
2018-04-12 16:20:46 -0500

    add eval pipeline

--- eval.sh
+++ eval.sh
@@ -1,3 +1,8 @@
+# Generate training, testing pickle file
+echo "Generating mention split data 90% training, 10% testing.."
 python ./src/generate_data.py --input=./data/smaller_preprocessed_sentence_keywords_labeled.tsv
-CUDA_VISIBLE_DEVICES=6,7 python ./src/BLSTM_train.py --corpus=./data/smaller_preprocessed_sentence_keywords_labeled.tsv --pre=False
+
+# Start train the model
+echo "Start Train the model.."
+CUDA_VISIBLE_DEVICES=0 python ./src/BLSTM_train.py --pre=False
 
